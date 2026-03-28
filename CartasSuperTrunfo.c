8adada9edd535c8084ac593f2534636af55938e6
Daniel Lopes Sampaio <dlsampaio2014@gmail.com>
2026-03-28 00:38:37 +0000

    Testando para codar.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,30 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+    int idade, matricula; // Posso fazer está declaração individual ou junta
+    float altura;
+    char nome[20];
+
+// Solicitação da idade
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+// Solicitação da altura
+    printf("Digite sua altura: ");
+    scanf("%.2f", &altura);
+
+// Solicitação do Nome
+    printf("Digite seu nome: ");
+    scanf("%s", &nome);
+
+// Solicitação da matricula
+    printf("Digite sua matricula: ");
+    scanf("%d", &matricula);
+
+// Saída de dados solicitados
+    printf("O aluno se chama: %s e sua matricula e de numero: %d \n", nome, matricula);
+    printf("Sua idade e de: %d anos e tem altura de: %.2f metros");
+
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
